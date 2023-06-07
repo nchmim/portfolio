d680ebd877309636ba48ae91d84d14c3e59c87e9
eric98zhang <eric99.zhang@gmail.com>
2023-06-07 15:23:50 -0700

    Try deploying to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/nchmim',
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
